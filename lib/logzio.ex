0b1e5f00033c490a9b151a49791177c7f2eaabac
Sreenadh TC <kesav.tc8@gmail.com>
2022-08-02 17:01:23 +0530

    send logs to logzio using http-bulk api

--- lib/logzio.ex
+++ lib/logzio.ex
@@ -1,18 +1,40 @@
 defmodule Logzio do
-  @moduledoc """
-  Documentation for `Logzio`.
-  """
+  @logzio_type "http-bulk"
 
-  @doc """
-  Hello world.
+  def send(messages) do
+    case HTTPoison.post(url(), messages, default_headers()) do
+      {:ok, %HTTPoison.Response{status_code: code, body: body}} ->
+        unless code in 200..299 do
+          IO.warn(
+            "Logzio API warning: Dropping Logs: HTTP response status is #{code}. Response body is: #{inspect(body)}"
+          )
+        end
 
-  ## Examples
+      {:error, reason} ->
+        IO.warn(
+          "Logzio API warning: Dropping Logs: HTTP request failed due to: #{inspect(reason)}"
+        )
+    end
+  end
 
-      iex> Logzio.hello()
-      :world
+  def url() do
+    "#{config(:base_url)}?token=#{config(:token)}&type=#{@logzio_type}"
+  end
 
-  """
-  def hello do
-    :world
+  def default_headers do
+    [
+      {"content-type", "text/plain"},
+      {"user-agent", "elixir-client/v#{Application.spec(:logzio, :vsn)}"}
+    ]
+  end
+
+  defp config(:token) do
+    configs = Application.get_env(:logger, :logzio)
+    Keyword.fetch!(configs, :token)
+  end
+
+  defp config(:base_url) do
+    configs = Application.get_env(:logger, :logzio)
+    Keyword.fetch!(configs, :base_url)
   end
 end
